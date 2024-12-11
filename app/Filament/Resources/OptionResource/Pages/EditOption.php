983d9f0224feeb0277671b4d999569802f43cfe9
Aqsha Rheyznandya Malik <aqsharheyznandyamalik@mail.ugm.ac.id>
2024-12-11 22:34:07 +0300

    Add Option and Question resources in Filament

---
+++ app/Filament/Resources/OptionResource/Pages/EditOption.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\OptionResource\Pages;
+
+use App\Filament\Resources\OptionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditOption extends EditRecord
+{
+    protected static string $resource = OptionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Resources/QuestionResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\QuestionResource\Pages;
+use App\Filament\Resources\QuestionResource\RelationManagers;
+use App\Models\Question;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class QuestionResource extends Resource
+{
+    protected static ?string $model = Question::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-question-mark-circle';
+
+    protected static ?string $navigationLabel = 'Exam Questions';
+
+    protected static ?string $modelLabel = 'Question';
+
+    protected static ?string $navigationGroup = 'Lesson Management';
+
+    protected static ?int $navigationSort = 4;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Textarea::make('question_text')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('exam_id')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('exam_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQuestions::route('/'),
+            'create' => Pages\CreateQuestion::route('/create'),
+            'edit' => Pages\EditQuestion::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
